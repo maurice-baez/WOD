3e82e6f3ea790d05e8c52bee8e3fccdd29a0e738
Maurice <maurice.baez1@gmail.com>
2022-06-19 14:22:40 -0700

    removed unneccasry comments

--- daily_wod_sms.py
+++ daily_wod_sms.py
@@ -9,35 +9,35 @@ import time
 
 load_dotenv()
 
-#Twilio authentication
+# Twilio authentication
 ACCOUNT_SID = os.environ['ACCOUNT_SID']
 AUTH_TOKEN = os.environ['AUTH_TOKEN']
 
 CF_URL = "https://www.crossfit.com"
-    
+
+
 def get_and_send_wod():
 
-    cfHomepage = requests.get(CF_URL)      ### Get raw HTML from Crossfit.com homepage
+    cfHomepage = requests.get(CF_URL)
 
-    soup = BeautifulSoup(cfHomepage.content, 'html.parser')    ### Create Beautiful Soup Object 
-    results = soup.find("div", class_="_workout-of-the-day-content_18rmn_60")     ### Grab workout area div
-    wod = results.find("article")       ### Remove "Workout of the day with comment count, return just the workout"
+    soup = BeautifulSoup(cfHomepage.content, 'html.parser')
+    results = soup.find("div", class_="_workout-of-the-day-content_18rmn_60")
+    wod = results.find("article")
 
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
-    client = Client(ACCOUNT_SID, AUTH_TOKEN)     ### Create Twilio client
-
-    message = client.messages.create(         ### Create text message
-                                body = f"Workout of the Day \n {wod.text}",
-                                from_='+18312492701',
-                                to='+18452421708'
-                            )
+    message = client.messages.create(
+        body=f"Workout of the Day \n {wod.text}",
+        from_='+18312492701',
+        to='+18452421708'
+    )
 
     print(message.sid)
 
     print(f"Workout of the Day \n {wod.text}")
 
 
-schedule.every().day.at("08:00").do(get_and_send_wod)   ### Call function every day @ 8am
+schedule.every().day.at("08:00").do(get_and_send_wod)
 
 
 while True:
